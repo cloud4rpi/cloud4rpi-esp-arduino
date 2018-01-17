1270899c3c098cd432765a01212e51e9c948b660
Sky3d <sky3ddd@gmail.com>
2018-01-17 17:06:32 +0300

    update nodemcu main sample

--- examples/nodemcu/src/main.cpp
+++ examples/nodemcu/src/main.cpp
@@ -12,19 +12,17 @@ int ledPin = BUILTIN_LED;
 int timerCountDown = 0;
 const int  publishPeriod = 15; // in sec
 
-const int featureCount = 6;
-String features[featureCount] = {
-    "Real time messaging using the MQTT protocol",
-    "Monitor device connection status",
-    "View device diagnostic data",
-    "Real time dashboard",
-    "Store and view data history",
-    "Alerts"
+const int eventCount = 3;
+String events[eventCount] = {
+    "IDLE",
+    "RING",
+    "BOOM!"
 };
 
 WiFiClient wifiClient;
+
 void ensureWiFiConnection();
-bool onLEDCommand(bool newValue);
+bool onLEDCommand(bool value);
 
 void setup() {
     Serial.begin(9600);
@@ -39,7 +37,7 @@ void setup() {
 
     c4r.declareBoolVariable("LEDOn", onLEDCommand);
     c4r.declareNumericVariable("Uptime");
-    c4r.declareStringVariable("Feature");
+    c4r.declareStringVariable("State");
 
     c4r.publishConfig();
     c4r.loop();
@@ -51,7 +49,8 @@ void loop() {
     ensureWiFiConnection();
     if (c4r.ensureConnection(3)) { // number of attempts
         c4r.setVariable("Uptime", millis());
-        c4r.setVariable("Feature", features[random(featureCount)]);
+        String newEvent = events[random(eventCount)];
+        c4r.setVariable("State", newEvent);
 
         c4r.publishData();
 
@@ -60,8 +59,8 @@ void loop() {
         Serial.println(c4r.getBoolValue("LEDOn"));
         Serial.print("Uptime = ");
         Serial.println(c4r.getNumericValue("Uptime"));
-        Serial.print("Feature = ");
-        Serial.println(c4r.getStringValue("Feature"));
+        Serial.print("State = ");
+        Serial.println(newEvent);
 
         timerCountDown = publishPeriod;
         while(timerCountDown--) {
